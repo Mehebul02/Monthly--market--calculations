e0b5411dac1ed6ffdd94a52900e6aa72ca75cf25
Mehebul02 <mehebul2123@gmail.com>
2024-02-16 23:30:37 +0600

    5% calculator

---
+++ index.js
@@ -0,0 +1,18 @@
+const btnSubmit=document.getElementById('btn_submiit')
+btnSubmit.addEventListener('click',function(){
+    // // // 1st cgpa 
+    //  const firstCgpa=document.getElementById('1st_cgpa')
+    //  const values=firstCgpa.value
+    // const total=parseFloat(values) *100/5;
+    // console.log(total)
+    
+    // // // 2nd cgpa 
+    // const secondCgpa=document.getElementById('2nd_cgpa').value
+    //  const secondCgpaTotal=parseFloat(secondCgpa)*100/5
+   
+    calculateTotal("1st_cgpa")
+    calculateTotal("2nd_cgpa")
+    calculateTotal("3rd_cgpa")
+
+   
+})
---
+++ utitlity.js
@@ -0,0 +1,6 @@
+
+function calculateTotal(id){
+    const values=document.getElementById(id).value
+     const total=parseFloat(values) *100/5;
+     console.log(total)
+}
