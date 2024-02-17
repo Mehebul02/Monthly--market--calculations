2cd829d7fdb1fe65ff8ff34d855b4ee8f0ae5b90
Mehebul02 <mehebul2123@gmail.com>
2024-02-17 12:06:41 +0600

    Diploma calculator campilite

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-const btnSubmit=document.getElementById('btn_submiit')
-btnSubmit.addEventListener('click',function(){
-    // // // 1st cgpa 
-    //  const firstCgpa=document.getElementById('1st_cgpa')
-    //  const values=firstCgpa.value
-    // const total=parseFloat(values) *100/5;
-    // console.log(total)
-    
-    // // // 2nd cgpa 
-    // const secondCgpa=document.getElementById('2nd_cgpa').value
-    //  const secondCgpaTotal=parseFloat(secondCgpa)*100/5
-   
-    calculateTotal("1st_cgpa")
-    calculateTotal("2nd_cgpa")
-    calculateTotal("3rd_cgpa")
-    tenPregnant('4th_cgpa')
-    fiveteenPregnant('5th_cgpa')
+const btnSubmit = document.getElementById("btn_submiit");
+btnSubmit.addEventListener("click", function () {
+  calculateTotal("1st_cgpa");
+  calculateTotal("2nd_cgpa");
+  calculateTotal("3rd_cgpa");
+  tenPregnant("4th_cgpa");
+  fiveteenPregnant("5th_cgpa");
+  twentyPregnant("6th_cgpa");
+  twentyFivePregnant("7th_cgpa");
+  fiveteenPregnant("8th_cgpa");
 
-   
-})
+  
+ const totalCgpa1 =
+ calculateTotal("1st_cgpa") +
+ calculateTotal("2nd_cgpa") +
+ calculateTotal("3rd_cgpa") +
+ tenPregnant("4th_cgpa") +
+ fiveteenPregnant("5th_cgpa") +
+ twentyPregnant("6th_cgpa") +
+ twentyFivePregnant("7th_cgpa") +
+ fiveteenPregnant("8th_cgpa");
+ console.log(totalCgpa1)
+ 
+setInnerText("total_cgpa",totalCgpa1.toFixed(2))
+setInnerText("grade_point",cgpaToGrade(totalCgpa1.toFixed(2)))
+// cgpaToGrade("grade_point")
+
+
+  
+ 
+});
+
--- utitlity.js
+++ utitlity.js
@@ -1,20 +1,61 @@
 // 5%
-function calculateTotal(id){
-    const values=document.getElementById(id).value
-     const total=parseFloat(values) *100/5;
-     console.log(total)
+function calculateTotal(id) {
+  const values = document.getElementById(id).value;
+  const total = (parseFloat(values) * 5) / 100;
+  console.log(total);
+  return total;
 }
 // 10%
-function tenPregnant(id){
-    const tenPregnantText=document.getElementById(id).value
-    const tenPregnantTotal=parseFloat(tenPregnantText)*100/10;
-    console.log(tenPregnantTotal);
-
+function tenPregnant(id) {
+  const tenPregnantText = document.getElementById(id).value;
+  const tenPregnantTotal = (parseFloat(tenPregnantText) * 10) / 100;
+  console.log(tenPregnantTotal);
+  return tenPregnantTotal;
 }
 // 15%
-function fiveteenPregnant(id){
-    const fiveteenPregnantText=document.getElementById(id).value
-    const fiveteenPregnantTotal=parseFloat(fiveteenPregnantText)*100/15
-    console.log(fiveteenPregnantTotal)
+function fiveteenPregnant(id) {
+  const fiveteenPregnantText = document.getElementById(id).value;
+  const fiveteenPregnantTotal = (parseFloat(fiveteenPregnantText) * 15) / 100;
+  console.log(fiveteenPregnantTotal);
+  return fiveteenPregnantTotal;
 }
 // 20%
+function twentyPregnant(id) {
+  const twentyPregnantText = document.getElementById(id).value;
+  const twentyPregnantTotal = (parseFloat(twentyPregnantText) * 20) / 100;
+  console.log(twentyPregnantTotal);
+  return twentyPregnantTotal;
+}
+// 25%
+function twentyFivePregnant(id) {
+  const twentyFivePregnantValue = document.getElementById(id).value;
+  const twentyFivePregnantTotal =
+    (parseFloat(twentyFivePregnantValue) * 25) / 100;
+  console.log(twentyFivePregnantTotal);
+  return twentyFivePregnantTotal;
+}
+
+// setInner Text
+function setInnerText(id, value) {
+  document.getElementById(id).innerText = value;
+}
+
+function cgpaToGrade(cgpa) {
+  let totalCgpa = parseFloat(cgpa);
+  console.log(typeof (totalCgpa))
+  if (totalCgpa == 4) {
+    return "A+";
+  } else if (totalCgpa >= 3.5 && totalCgpa < 4) {
+    return "A";
+  } else if (totalCgpa >= 3 && totalCgpa < 3.5) {
+    return "A-";
+  } else if (totalCgpa >= 2.5 && totalCgpa < 3) {
+    return "B";
+  } else if (totalCgpa >= 2 && totalCgpa < 2.5) {
+    return "B-";
+  } else if (totalCgpa >= 1.5 && totalCgpa < 2) {
+    return "C";
+  } else {
+    return "Fail";
+  }
+}
